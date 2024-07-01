4eb98dd6f2c80de6b98b0fd838065a1b2e3fee21
Softjey <misivsvatoslav@gmail.com>
2024-07-01 19:15:37 +0200

    add .env vars for redis

--- src/modules/config/env-variable.enum.ts
+++ src/modules/config/env-variable.enum.ts
@@ -2,6 +2,7 @@ const enum EnvVariable {
   MOVIE_API_KEY = 'MOVIE_API_KEY',
   MOVIE_API_BASE_URL = 'MOVIE_API_BASE_URL',
   MOVIE_API_IMAGE_URL = 'MOVIE_API_IMAGE_URL',
+  REDIS_URL = 'REDIS_URL',
 }
 
 export default EnvVariable;
