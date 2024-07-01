0d66a338342a0fa508db7be3bd101e2a33489215
Softjey <misivsvatoslav@gmail.com>
2024-07-01 17:32:02 +0200

    rename env variables enum file, add new env variables

--- src/modules/config/config.types.ts
+++
@@ -1,5 +0,0 @@
-const enum EnvVariable {
-  API_KEY = 'MOVIE_API_KEY',
-}
-
-export default EnvVariable;
---
+++ src/modules/config/env-variable.enum.ts
@@ -0,0 +1,7 @@
+const enum EnvVariable {
+  MOVIE_API_KEY = 'MOVIE_API_KEY',
+  MOVIE_API_BASE_URL = 'MOVIE_API_BASE_URL',
+  MOVIE_API_IMAGE_URL = 'MOVIE_API_IMAGE_URL',
+}
+
+export default EnvVariable;
