0c1713101d0c7f913ab4f75adf81b017cb8083c4
Softjey <misivsvatoslav@gmail.com>
2024-07-01 15:56:41 +0200

    remove unnecessary app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MovieModule } from './modules/movie/movie.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), MovieModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
